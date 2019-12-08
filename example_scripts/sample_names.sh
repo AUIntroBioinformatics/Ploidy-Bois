fff07e1a8021797957eb7052b55cc4fc1556ceb3
Sarah Martin <32333699+sem0036@users.noreply.github.com>
2019-12-08 16:24:51 -0600

    Create sample_names.sh

---
+++ example_scripts/sample_names.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#Sample names from generated genomes must be consistent with Illumina sequencing for output of bwa
+mv 0SNP_1.dwgsim.0.12.fastq 0SNP_S1_L001_R1_001.fastq
+mv 0SNP_2.dwgsim.0.12.fastq 0SNP_S2_L001_R1_001.fastq
+mv 0SNP_3.dwgsim.0.12.fastq 0SNP_S3_L001_R1_001.fastq
+mv 100SNP_1.dwgsim.0.12.fastq 100SNP_S1_L001_R1_001.fastq
+mv 100SNP_2.dwgsim.0.12.fastq 100SNP_S2_L001_R1_001.fastq
+mv 100SNP_3.dwgsim.0.12.fastq 100SNP_S3_L001_R1_001.fastq
+mv 10kSNP_1.dwgsim.0.12.fastq 10kSNP_S1_L001_R1_001.fastq
+mv 10kSNP_2.dwgsim.0.12.fastq 10kSNP_S2_L001_R1_001.fastq
+mv 10kSNP_3.dwgsim.0.12.fastq 10kSNP_S3_L001_R1_001.fastq
+mv 50kSNP_1.dwgsim.0.12.fastq 50kSNP_S1_L001_R1_001.fastq
+mv 50kSNP_2.dwgsim.0.12.fastq 50kSNP_S2_L001_R1_001.fastq
+mv 50kSNP_3.dwgsim.0.12.fastq 50kSNP_S3_L001_R1_001.fastq
