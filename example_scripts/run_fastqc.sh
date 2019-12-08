d8509ee19b7b45c5c2fa8d1a2c681546f0d294e7
Sarah Martin <32333699+sem0036@users.noreply.github.com>
2019-12-08 16:26:42 -0600

    Update run_fastqc.sh

--- example_scripts/run_fastqc.sh
+++ example_scripts/run_fastqc.sh
@@ -2,7 +2,12 @@
 
 module load fastqc
 
+#Ploidy (Kyle)
 fastqc 0.2X/0.2X_reads/config1_concat.fastq
 fastqc 0.4X/0.4X_reads/config1_concat.fastq
 fastqc 0.8X/0.8X_reads/config1_concat.fastq
 fastqc 1.6X/1.6X_reads/config1_concat.fastq
+
+#SNP (Sarah)
+cd ../..//scratch/ploidy_boids
+fastqc 100SNP_*.dwgsim.0.12.fastq --outdir=/home/aubcls15/Step_1
